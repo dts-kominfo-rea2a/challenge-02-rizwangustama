f3b67ff23774d65d35dd91345b5c1b0f0aaa93d6
rizwangustama <rizonegustama@gmail.com>
2022-05-28 16:35:44 +0700

    update

--- index.js
+++ index.js
@@ -23,19 +23,35 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+  let hasilLooping = [];
+  dataYangAkanDilooping.forEach(function (item) {
+    hasilLooping.push(item.namaDepan + " " + item.namaBelakang);
+  });
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
+  
+  let jumlahPria = [];
+  dataYangAkanDilooping.forEach(function (item) {
+    if (item.jenisKelamin === "M") {
+        jumlahPria++;
+      }
+    }
+  );
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
+  let jumlahWanita = [];
+  dataYangAkanDilooping.forEach(function (item) {
+    if (item.jenisKelamin === "F") {
+        jumlahWanita++;
+      }
+    }
+  );
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -48,15 +64,24 @@ function lakukanLooping(arrPegawai) {
       Apabila imbang, maka komentar akan berisi:
         "Jumlah Pria dan Wanita berimbang"
   */
-  let komentar = null;
+  let komentar = "";
+  if (jumlahPria > jumlahWanita) {
+    komentar = "Jumlah Pria lebih banyak dari Wanita";
+  }
+  else if (jumlahWanita > jumlahPria) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria";
+  }
+  else {
+    komentar = "Jumlah Pria dan Wanita berimbang";
+  }
 
-  // ! JANGAN DIMODIFIKASI
-  return {
-    hasilLooping,
-    jumlahPria,
-    jumlahWanita,
-    komentar,
-  };
+// ! JANGAN DIMODIFIKASI
+return {
+  hasilLooping,
+  jumlahPria,
+  jumlahWanita,
+  komentar,
+};
 }
 
 function main(data) {
